fcf8fd1a7a5770d5deb5975783f36d4f25f74f76
我的前半生 <work225@126.com>
2026-06-18 22:05:11 +0800

    优化

--- frontend/src/views/FileConverter.vue
+++ frontend/src/views/FileConverter.vue
@@ -162,7 +162,11 @@
       <div class="table-wrap">
         <el-table :data="historyTasks" v-loading="historyLoading" stripe>
           <el-table-column prop="id" label="ID" width="70" />
-          <el-table-column prop="output_filename" label="文件名" min-width="200" show-overflow-tooltip />
+          <el-table-column prop="filename" label="文件名" min-width="200" show-overflow-tooltip >
+            <template #default="{ row }">
+              {{ row.output_filename || row.filename }}
+            </template>
+          </el-table-column>
           <el-table-column label="类型" width="140">
             <template #default="{ row }">
               <el-tag size="small" effect="plain" :type="getTypeTag(row.conversion_type)">
